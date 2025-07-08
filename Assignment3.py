735612c496e60c085e9eb0ae686b831e1b152b09
aakriysng <aakritsingh9105@gmail.com>
2025-07-08 11:57:50 +0530

    Initial project upload

---
+++ Assignment3.py
@@ -0,0 +1,23 @@
+# Task -->1
+def factorial(n):
+    if n < 2:
+        return 1
+
+    else:
+        return n * (factorial(n-1))
+    
+num = int(input("enter a number: "))
+result = factorial(num)
+print(f"Factorial of {num} is : {result} ")
+
+# Task -->2
+
+import math
+
+def number():
+    n = int(input("enter the number: "))
+    print("square root :",n ** 0.5)
+    print("Logarithm : ", math.log(n))
+    print("Sine : ",math.sin(n))
+
+number()
